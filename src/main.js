4ef834604e4ad4e8e780dfc79bb150c6f084b5d9
Elaine <elaine85leal@gmail.com>
2025-03-03 20:57:43 -0500

    server

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import setupFormHandler from './modules/FormHandler.js';
+import setupFormHandler from './js/modules/FormHandler.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   setupFormHandler(); // configurar el listener del formulario
